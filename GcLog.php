3d47b7266ccf3699ae707dd0714af80090c819b8
COCOQI <615817435@qq.com>
2020-10-26 15:25:21 +0800

    Create GcLog.php

---
+++ GcLog.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace GcLog;
+
+
+class GcLog
+{
+    private $projectName;
+    private $moduleName;
+    private $logFilePath;
+    private $logLevels;
+    public static function init($projectName,$moduleName,$logFilePath,$logLevels='info')
+    {
+
+    }
+
+    public function __construct()
+    {
+        //$this->$projectName = $projectName;
+    }
+
+    public static function inputLog()
+    {
+
+    }
+
+    public static function outputLog()
+    {
+
+    }
+
+    public static function error()
+    {
+
+    }
+
+    public function exception()
+    {
+
+    }
+
+    public function debug()
+    {
+
+    }
+
+    public function log()
+    {
+
+    }
+}
